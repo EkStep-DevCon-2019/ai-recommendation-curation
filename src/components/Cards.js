13c30b87d68bdb268834e43372293ca84c8cc21e
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2019-01-23 15:43:18 +0530

    Card design change

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -26,16 +26,16 @@ class Cards extends React.Component {
   render() {
     return (
       <div style={{margin: '10px'}}>
-        <Header style={{paddingTop: '20px', paddingLeft: '40px'}}>  
+        <Header style={{paddingTop: '20px', paddingLeft: '40px'}}>
           Recommended:
         </Header>
         <Card.Group style={{marginTop: '10px', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
           {this.state.results.map((x, i) =>
             <Card key={i}>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' style={{width: '100%'}}/>
               <Card.Content>
                 <Card.Header>{x}</Card.Header>
                 <Card.Meta>Friends of Elliot</Card.Meta>
-                <Image src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmusVyHLSxuIdiEyhcIKOM5qJ_VsMnMwx3VTwcf6j2QV5YGrpzMg' style={{width: '100%'}}/>
                 <Card.Description>
                   Steve wants to add you to the group <strong>best friends</strong>
                 </Card.Description>
@@ -48,7 +48,7 @@ class Cards extends React.Component {
         </Card.Group>
         <br />
         <Button primary floated='right' onClick={this.props.handleSubmit}> Submit </Button>
-        <br /> 
+        <br />
       </div>
     );
   }
