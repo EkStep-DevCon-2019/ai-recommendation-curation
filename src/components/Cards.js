7cc59360788c027b1f39494beba17fc442bbd003
ishawakankar <iwakankar@hotmail.com>
2019-01-23 16:10:37 +0530

    added cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { Card, Button, Image, Header, Rating } from 'semantic-ui-react';
-import API from '../utils/Api'
-
+import { Card, Button, Image, Header, Rating, Dimmer, Loader, Segment } from 'semantic-ui-react';
 
 class Cards extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      results: ['Title1', 'Title2', 'Title3'],
-    };
   }
 
   componentDidMount = () => {
@@ -29,23 +24,27 @@ class Cards extends React.Component {
         <Header style={{paddingTop: '20px', paddingLeft: '40px'}}>  
           Recommended:
         </Header>
+        {(this.props.tags.length==0)? <Dimmer active inverted style={{marginTop: '80px' }}>
+            <Loader size='massive'>Loading Search Results</Loader>
+          </Dimmer> :
         <Card.Group style={{marginTop: '10px', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-          {this.state.results.map((x, i) =>
+          {this.props.tags.map((x, i) =>
             <Card key={i}>
               <Card.Content>
-                <Card.Header>{x}</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Image src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmusVyHLSxuIdiEyhcIKOM5qJ_VsMnMwx3VTwcf6j2QV5YGrpzMg' style={{width: '100%'}}/>
+                <Card.Header>{x.name}</Card.Header>
+                {/* <Card.Meta>Friends of Elliot</Card.Meta> */}
+                {/* <Image src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmusVyHLSxuIdiEyhcIKOM5qJ_VsMnMwx3VTwcf6j2QV5YGrpzMg' style={{width: '100%'}}/> */}
+                <Image src={x.Previewurl} alt="Image not available" style={{width: '100%', height: 200}} />
                 <Card.Description>
                   Steve wants to add you to the group <strong>best friends</strong>
                 </Card.Description>
               </Card.Content>
               <Card.Content extra>
-                <Rating icon='star' maxRating={5} onRate={(e,{rating}) => this.handleRate(e,{rating},i)} />
+                <Rating icon='star' maxRating={5} onRate={(e,{rating}) => this.handleRate(e,{rating},x.id)} />
               </Card.Content>
             </Card>
           )}
-        </Card.Group>
+        </Card.Group>}
         <br />
         <Button primary floated='right' onClick={this.props.handleSubmit}> Submit </Button>
         <br /> 
